77ca8ad7af5cdd6a4dff5b69085be22b19e4b0da
TOMIKE <kolajoelizabeth@gmail.com>
2024-05-03 15:21:38 +0100

    Arrays

--- helloWorld/script.js
+++ helloWorld/script.js
@@ -22,22 +22,33 @@ let school= "Funaab";
 
 // console.log(ageGap(500))
 
-function testSize(num) {
-  // Only change code below this line
-if(num < 5){
-  return "Tiny";
-} else if(num < 10) {
-  return "Small";
-} else if (num < 15){
-  return "Medium";
-} else if (num < 20){
-  return "Large";
-} else if (num >=20){
-  return "Huge";
-} else{
-  return "Change Me";
-}
-  // Only change code above this line
+// function testSize(num) {
+//   // Only change code below this line
+// if(num < 5){
+//   return "Tiny";
+// } else if(num < 10) {
+//   return "Small";
+// } else if (num < 15){
+//   return "Medium";
+// } else if (num < 20){
+//   return "Large";
+// } else if (num >=20){
+//   return "Huge";
+// } else{
+//   return "Change Me";
+// }
+//   // Only change code above this line
+// }
+
+// testSize(7);
+
+//ARRAYS
+let Friends = [];
+
+function addMyFriends(friend){
+Friends.push(friend);
+console.log(Friends)
 }
 
-testSize(7);
+addMyFriends('tomi');
+addMyFriends('stella');
