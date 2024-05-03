0125c6ea4025879e1231c6d777391cb8b63b68e8
TOMIKE <kolajoelizabeth@gmail.com>
2024-05-03 11:35:28 +0100

    if-else-statement

--- helloWorld/script.js
+++ helloWorld/script.js
@@ -1,1 +1,23 @@
-console.log('hi')
+let age = 23;
+let name = "tomike";
+let school= "Funaab";
+
+
+
+// function addTotal(incrementby , multiplyby) {
+//     return (age + incrementby ) * multiplyby;
+// }
+
+// addTotal(5,2);
+// console.log(age)
+
+//if-else statements
+function ageGap(myage){
+    if (myage > 30){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(ageGap(500))
