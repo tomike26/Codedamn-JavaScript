f6ebb2f193a9874bd6ad0f50b9e4272a2e293ac4
TOMIKE <kolajoelizabeth@gmail.com>
2024-05-03 13:42:46 +0100

    if-else-statement

--- helloWorld/script.js
+++ helloWorld/script.js
@@ -12,12 +12,32 @@ let school= "Funaab";
 // console.log(age)
 
 //if-else statements
-function ageGap(myage){
-    if (myage > 30){
-        return true;
-    }else {
-        return false;
-    }
+// function ageGap(myage){
+//     if (myage > 30){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+
+// console.log(ageGap(500))
+
+function testSize(num) {
+  // Only change code below this line
+if(num < 5){
+  return "Tiny";
+} else if(num < 10) {
+  return "Small";
+} else if (num < 15){
+  return "Medium";
+} else if (num < 20){
+  return "Large";
+} else if (num >=20){
+  return "Huge";
+} else{
+  return "Change Me";
+}
+  // Only change code above this line
 }
 
-console.log(ageGap(500))
+testSize(7);
