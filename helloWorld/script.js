059f5b5e7f17cef16dc4884557849cc5a83363c1
TOMIKE <kolajoelizabeth@gmail.com>
2024-05-06 11:18:38 +0100

    break statement

--- helloWorld/script.js
+++ helloWorld/script.js
@@ -43,17 +43,34 @@ let school= "Funaab";
 // testSize(7);
 
 //ARRAYS
-let Friends = [];
+// let Friends = [];
 
-function addMyFriends(friend){
-Friends.push(friend);
-console.log(Friends)
-}
+// function addMyFriends(friend){
+// Friends.push(friend);
+// console.log(Friends)
+// }
 
-addMyFriends('tomi');
-addMyFriends('stella');
+// addMyFriends('tomi');
+// addMyFriends('stella');
 
 //For loop
-for (i = 0; i < 10; i++){
-    console.log(i)
+// for (i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+
+//how to use break and continue
+let evenNumbers = [];
+let oddNumbers = [];
+
+
+for (let i = 0; i <20; i++){
+    if(i % 2 === 0){
+        evenNumbers.push(i);
+    } else{
+        oddNumbers.push(i);
+    }
 }
+
+
+console.log(evenNumbers, oddNumbers);
